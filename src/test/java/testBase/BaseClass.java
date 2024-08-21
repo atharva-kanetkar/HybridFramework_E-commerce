b22d7577c291777fc84615a38f0a8665e23b1bf8
Athrva <atharvakanetkar99@gmail.com>
2024-08-22 03:03:16 +0530

    Added cross browser testing and config property file

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,13 +1,18 @@
 package testBase;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
-
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.apache.logging.log4j.LogManager; //log4j
 import org.apache.logging.log4j.Logger; //log4j
 
@@ -15,23 +20,41 @@ public class BaseClass {
 
 	public WebDriver driver;
 	public Logger logger;
+	public Properties p; // imported from java.utils for loading config file
 	
-	String url = "https://tutorialsninja.com/demo/" ;
 	
 	@BeforeClass
-	public void setup() {
+	@Parameters({"os", "browser"}) // coming from master.xml and passed into setup method
+	public void setup(String os, String browser) throws IOException {
 		
+		
+		
+		//Loading config file 
+		FileReader file = new FileReader("./src//test//resources//config.properties");
+		p = new Properties(); // created object of properties
+		p.load(file); //loading file with hel of properties
+		
+		
+		//Logger
 		logger = LogManager.getLogger(this.getClass()); //it will fetch the xml file into logger variabe
 		
 		
+		switch (browser.toLowerCase()) 
+		{
+		case "chrome" : driver = new ChromeDriver(); break;
+		case "edge" : driver = new EdgeDriver(); break;
+		case "firefox" : driver = new FirefoxDriver(); break;	
+		default : System.out.println("Invalid browser name"); return;
+		}
+		
+		//we have passed parameter from master.xml to baseclass above
 		
 		
-		
-		driver = new ChromeDriver();
-		driver.get(url);
+		driver.get(p.getProperty("appURL")); // reading value from config property file
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().deleteAllCookies();
+		
 	
 	}
 	
